4dc110ebd6931a9d66cdac470c659e7703a67b4d
Eugenio <eugenio.sanchez@gmail.com>
2023-03-14 19:50:23 +0100

    Ejercicio arrays

---
+++ 3.Arrays/1.Ejercicio.js
@@ -0,0 +1,13 @@
+//Donat un array de pel·lícules nominades als Óscars
+//
+//a) Recorr l'array i escriu per pantalla la pel·lícula que té més de 10
+//   caracters. Pista: mètode .length
+
+
+const oscars=['Everything Everywhere All at Once','Top Gun','Ellas hablan']
+
+for (let i = 0; i<oscars.length; i++){
+   if (oscars[i].length > 10)
+	console.log(oscars[i]
+}
+
